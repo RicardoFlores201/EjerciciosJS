24a9c77ca932e272b0103ae21a9c969b11c19c2c
richardram233 <richardram233@gmail.com>
2025-07-29 22:57:01 -0600

    03-PT-React-P1

---
+++ 03-PT-React/PT-React/src/app.jsx
@@ -0,0 +1,27 @@
+import { useEffect, useState } from "react";
+
+// const CAT_ENDPOINT_IMAGE_URL = `https://cataas.com/cat/says/${firstWord}?size=50&color=red&json=true`
+const CAT_ENDPOINT_RANDOM_FACT = 'https://catfact.ninja/fact'
+
+export function App () {
+    const [fact, setFact] = useState()
+
+    useEffect(() => {
+        fetch(CAT_ENDPOINT_RANDOM_FACT)
+        .then(res => res.json())
+        .then(data => setFact(data.fact))
+
+    }, [])
+
+    
+
+    return (
+        <main>
+            <h2>TEST</h2>
+            {fact && <p>
+                {fact}
+            </p>}
+        </main>
+
+    );
+}
